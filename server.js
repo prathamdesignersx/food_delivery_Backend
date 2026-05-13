c3dbc740b79f8d54de1a452f336cf891874356f9
prathamdesignersx <pratham.s@designersx.com>
2026-05-13 16:25:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-  res.send("API Working");
+  res.send("API  not Working");
 });
 
 app.listen(port, () => {
