28c9ecce0588e0a72ecb09f4fcb02806f02f3b25
Pratham <prathamsharma1972@gmail.com>
2026-05-18 12:35:31 +0530

    regenerated pipelines

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-  res.send("API  is always Working");
+  res.send("API  is alwayss Working");
 });
 
 app.listen(port, () => {
