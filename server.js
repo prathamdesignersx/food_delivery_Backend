a2dd95c80147d152df3e3cabf79ce8e30661de45
Pratham <prathamsharma1972@gmail.com>
2026-05-18 11:17:47 +0530

    msg

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-  res.send("API  not Working");
+  res.send("API  is Working");
 });
 
 app.listen(port, () => {
